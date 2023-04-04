6b20a6da6095a94e4a6688fe1d2a6c201716b881
M-Farjad <2021se56@student.uet.edu.pk>
2023-04-04 17:54:11 +0500

    AppBar & Floating Button

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart.';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(CupertinoIcons.home),
+          onPressed: () {},
+        ),
+        title: const Text("Livchat"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add_reaction_outlined),
+      ),
+    );
+  }
+}
